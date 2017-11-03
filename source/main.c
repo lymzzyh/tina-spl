737e2a47efbb534fe53afe987122b1ea5b27afed
Jiang Jianjun <8192542@qq.com>
2017-11-04 02:18:06 +0800

    working on f1c100s

--- source/main.c
+++ source/main.c
@@ -1,26 +1,50 @@
 #include <xboot.h>
 
-static void  aw_delay(volatile unsigned n)
+static void __delay(volatile uint32_t loop)
 {
-	while(n)
-	{
-		n--;
-		__asm__ __volatile__("nop" ::: "memory", "cc");
-		__asm__ __volatile__("nop" ::: "memory", "cc");
-		__asm__ __volatile__("nop" ::: "memory", "cc");
-		__asm__ __volatile__("nop" ::: "memory", "cc");
-		__asm__ __volatile__("nop" ::: "memory", "cc");
-		__asm__ __volatile__("nop" ::: "memory", "cc");
-		__asm__ __volatile__("nop" ::: "memory", "cc");
-		__asm__ __volatile__("nop" ::: "memory", "cc");
-		__asm__ __volatile__("nop" ::: "memory", "cc");
-		__asm__ __volatile__("nop" ::: "memory", "cc");
-		__asm__ __volatile__("nop" ::: "memory", "cc");
-		__asm__ __volatile__("nop" ::: "memory", "cc");
-		__asm__ __volatile__("nop" ::: "memory", "cc");
-		__asm__ __volatile__("nop" ::: "memory", "cc");
-		__asm__ __volatile__("nop" ::: "memory", "cc");
-	}
+	for(; loop > 0; loop--);
+}
+
+void udelay(uint32_t us)
+{
+	__delay(us * 2);
+}
+
+void mdelay(uint32_t ms)
+{
+	udelay(ms * 1000);
+}
+
+void led_init(void)
+{
+	virtual_addr_t addr;
+	u32_t val;
+
+	/* Config GPIOA0 to GPIO output and pull up */
+	addr = 0x01c20800 + 0x00;
+	val = read32(addr);
+	val &= ~(0xf << ((0 & 0x7) << 2));
+	val |= ((0x1 & 0x7) << ((0 & 0x7) << 2));
+	write32(addr, val);
+
+	addr = 0x01c20800 + 0x1c;
+	val = read32(addr);
+	val &= ~(0x1 << ((0 & 0xf) << 1));
+	val |= (0x1 << ((0 & 0xf) << 1));
+	write32(addr, val);
+}
+
+void led_set_level(int level)
+{
+	virtual_addr_t addr;
+	u32_t val;
+
+	/* Set GPIOA0 level */
+	addr = 0x01c20800 + 0x10;
+	val = read32(addr);
+	val &= ~(1 << 0);
+	val |= (!!level) << 0;
+	write32(addr, val);
 }
 
 int xboot_main(int argc, char * argv[])
@@ -37,24 +61,25 @@ int xboot_main(int argc, char * argv[])
 	fmt[7] = '\n';
 	fmt[8] = 0;
 
-	//gpio_set_cfg(GPIO_A(0), 1, 1);
-	//gpio_set_pull(GPIO_A(0), 1, 1);
+	led_init();
 
 	for(i = 0; i < 5; i++)
 	{
-		//gpio_set_val(GPIO_A(0), 0);
-		aw_delay(50000);
-		//gpio_set_val(GPIO_A(0), 1);
-		aw_delay(50000);
+		led_set_level(0);
+		mdelay(500);
+		led_set_level(1);
+		mdelay(500);
+
 		sys_uart_putc('X');
 	}
 
-	for(i = 0; i < 500000; i++)
+	for(i = 0; i < 10000; i++)
 	{
-		//gpio_set_val(GPIO_A(0), 0);
-		aw_delay(50000);
-	//	gpio_set_val(GPIO_A(0), 1);
-		aw_delay(50000);
+		led_set_level(0);
+		mdelay(500);
+		led_set_level(1);
+		mdelay(500);
+
 		printk(fmt, i);
 	}
 
